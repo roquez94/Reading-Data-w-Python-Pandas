e2c80ba31b5c6a8ed7ea7d6d2584f035ba3092bf
Roquez <roquez94@gmail.com>
2024-10-18 21:30:07 -0400

    Figured out how to import files needed

--- unit-1-reading-data-with-python-and-pandas/lesson-2-load-movies-dataset/main.py
+++ unit-1-reading-data-with-python-and-pandas/lesson-2-load-movies-dataset/main.py
@@ -1,7 +1,16 @@
-import numpy as np
 import pandas as pd
+import numpy as np
+
 
 column_names = ['color', 'director_name', 'num_critic_for_reviews', 'duration',
                 'gross', 'movie_title', 'num_user_for_reviews',	'country',
                 'cotent_rating', 'budget', 'title_year', 'imdb_score', 'genre']
 
+csv_url = 'movies.csv'
+df = pd.read_csv(csv_url, 
+                 sep ='|', names=column_names)
+
+print(df)
+df.head(10)
+
+df.describe()
