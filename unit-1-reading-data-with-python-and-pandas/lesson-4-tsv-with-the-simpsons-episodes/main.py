9d60455c37a378b304286ad4ee470814c278a27b
Roquez <roquez94@gmail.com>
2024-10-20 11:22:23 -0400

    Practice with tsv files, use sep='\t'

--- unit-1-reading-data-with-python-and-pandas/lesson-4-tsv-with-the-simpsons-episodes/main.py
+++ unit-1-reading-data-with-python-and-pandas/lesson-4-tsv-with-the-simpsons-episodes/main.py
@@ -1,7 +1,26 @@
 import numpy as np
 import pandas as pd
 
-col_names = ['Title', 'Air date', 'Production code', 'Season', 'Number in season',
+column_names = ['Title', 'Air date', 'Production code', 'Season', 'Number in season',
              'Number in series', 'US viewers (million)', 'Views', 'IMDB rating']
-             
-                          
+
+simpsons = pd.read_csv('unit-1-reading-data-with-python-and-pandas\lesson-4-tsv-with-the-simpsons-episodes\data\simpsons-episodes.tsv',
+                       sep = '\t',
+                       names = column_names,
+                       skiprows= 4,
+                       usecols= ['Title', 'Air date', 'Production code', 'IMDB rating'],
+                       na_values='no_val',
+                       index_col='Production code',
+                       parse_dates=['Air date'])
+
+simpsons.describe()
+print(simpsons)
+
+
+# Use correct separator as data is tabular separated.
+# - Use the following `col_names` list as column names.
+# - Load just `Title`, `Air date`, `Production code` and `IMDB rating`.
+# - Don't load the first empty columns.
+# - Set `Production code` as index.
+# - Null values are encoded as `no_val` values, be careful with that when loading the data.
+# - Parse the `Air date` columns as Date.
