c64652a16ca5b80e27e19cf79c4a124511212233
Roquez <roquez94@gmail.com>
2024-10-20 13:31:58 -0400

    practiced lesson 7, using different sheet names

--- unit-1-reading-data-with-python-and-pandas/lesson-6-reading-playstore-apps-excel-file/main.py
+++ unit-1-reading-data-with-python-and-pandas/lesson-6-reading-playstore-apps-excel-file/main.py
@@ -2,3 +2,22 @@ import numpy as np
 import pandas as pd
 
 data_url = 'https://github.com/ine-rmotr-projects/project-files/files/4086772/playstore.xlsx'
+
+playstore = pd.read_excel(data_url,
+                          usecols = ['App', 'Rating', 'Installs', 'Genres', 'Last_Updated'],
+                          parse_dates= ['Last_Updated']
+                          )
+
+print(playstore)
+
+print(playstore.nlargest(25,'Rating'))
+# playstore_df = playstore_df.sort_values('Rating', ascending=False).head(25)
+
+
+
+# Read the `playstore.xlsx` Excel file from the given `data_url` and store it in a `playstore_df` DataFrame.
+
+# - When reading in the file, only use the columns `'App', 'Rating', 'Installs', 'Rating', 'Genres', 'Last_Updated'`.
+# - Make sure `Last_Updated` is in datetime format, try do this while reading the file into the DataFrame.
+
+# After reading the data, filter the records and keep only the top 25 with highest `Rating` (being 5 the highest possible rating value).
--- unit-1-reading-data-with-python-and-pandas/lesson-7-load-playstore-apps-excel-file-with-multiple-sheets/main.py
+++ unit-1-reading-data-with-python-and-pandas/lesson-7-load-playstore-apps-excel-file-with-multiple-sheets/main.py
@@ -2,3 +2,17 @@ import numpy as np
 import pandas as pd
 
 data_url = 'https://github.com/ine-rmotr-projects/project-files/files/4086772/playstore.xlsx'
+
+playstore = pd.read_excel(data_url,
+                          index_col='App')
+
+contentID = pd.read_excel(data_url, sheet_name='Content_ID', index_col='Content_ID')
+
+print(playstore)
+
+print(contentID)
+
+# Using the `playstore.xlsx` Excel file from the given `data_url` and:
+
+# - Save in a `playstore_df` variable the `Google_playstore` sheet. Use the first column as index.
+# - Save in a `content_id_df` variable the `Content_ID` sheet. Use `Content_ID` as index.
